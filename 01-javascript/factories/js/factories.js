1003bcd7ecb74ba6821b27395f4a684cf07dc758
JT <certainthings@gmail.com>
2019-10-04 12:50:37 +1000

    cat factory and constructor examples

---
+++ 01-javascript/factories/js/factories.js
@@ -0,0 +1,54 @@
+// Factories Mass Produce Objects
+
+const catFactory = function (n, a, f) { // n: name, a: age, f: fur color
+  return {
+    name: n,
+    age: a,
+    furColor: f,
+    meow: function () {
+      console.log('Meow');
+    }
+  };
+};
+
+const cuteCats = [
+		// { name: "Angel", age: 18, furColor: "grey" },
+		// { name: "Evil", age: 14, furColor: "red" },
+		// { name: "Meh", age: 12, "Fur Color": "white" }
+    catFactory('Angel', 18, 'grey'),
+    catFactory('Evil', 14, 'red'),
+    catFactory('Meh', 12, 'white')
+];
+
+for (let i = 0; i < cuteCats.length; i++) {
+  const cat = cuteCats[i];
+  console.log(`${ cat.name } has ${ cat.furColor } fur.`);
+}
+
+// Prototypal Inheritance //////////////////////////////////////////////////////
+// Historically interesting but not essential (for now/or anymore).
+
+// Constructor
+const Cat = function (n, a, f) {
+  // No need to create the object: it's already there and it's called "this".
+  this.name = n;
+  this.age = a;
+  this.furColor = f;
+  this.meow = function () {
+    console.log('Meow');
+  };
+  // no need to return "this" -- it gets returned automagically.
+};
+
+// Instances
+const cooper = new Cat('Cooper', 2, 'grey');
+const audrey = new Cat('Audrey', 2, 'grey');
+
+// The main advantage of prototypes:
+// You can extend the prototype AFTER you create your "instances".
+Cat.prototype.species = 'feline';
+Cat.prototype.eat = function (food) {
+  console.log('Yum, I love to eat ' + food);
+};
+
+audrey.eat('gelatin');
