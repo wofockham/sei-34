44b57355c47c91623337f4a25274c6f1b80ebf00
JT <certainthings@gmail.com>
2019-10-11 12:45:59 +1100

    youtube with dom

---
+++ 04-jquery/youtube-i/js/main.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('li a');
+
+for (let i = 0; i < links.length; i++) {
+  const link = links[ i ];
+  const href = link.getAttribute('href');
+
+  const thumbnailURL = youtube.generateThumbnailUrl( href );
+
+  const image = document.createElement('img'); // Detached DOM node
+  image.setAttribute('src', thumbnailURL);
+
+  link.appendChild( image );
+}
