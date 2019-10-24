ea557de78abe60270e9a2faa87ed83ecde955021
JT <certainthings@gmail.com>
2019-10-24 13:01:11 +1100

    sql basics

---
+++ 07-databases/intro/pets.sql
@@ -0,0 +1,11 @@
+CREATE TABLE pets (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT,
+  age INTEGER,
+  dead BOOLEAN
+);
+
+-- seed data
+INSERT INTO pets (id, name, breed, age, dead) VALUES (1, 'Fluffy', 'unicorn', 1000, 0);
+INSERT INTO pets VALUES (2, 'Gigantor', 'robot', 1, 1);
