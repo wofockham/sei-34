c099ecbb4c67e0eaa6aaf1f6ca965b853a87512a
JT <certainthings@gmail.com>
2019-11-01 09:36:49 +1100

    sessions and login

---
+++ 08-rails/tunr/app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by :email => params[:email]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id # Remember the user id from this moment on.
+      redirect_to root_path
+    else
+      flash[:error] = "Invalid email address or password"
+      redirect_to login_path # We don't render on failure because we don't want to help a hacker.
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+end
--- 08-rails/tunr/app/controllers/users_controller.rb
+++ 08-rails/tunr/app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save # Returns truthy value on success
+      redirect_to root_path
+    else
+      render :new
+      # redirect_to new_user_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
