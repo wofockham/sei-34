35a32677f78ca2dcdb62462a58fd0f706ab5c4d3
JT <certainthings@gmail.com>
2019-11-04 15:42:08 +1100

    wow bootstrap is a nightmare on rails 6

--- 08-rails/tunr/app/javascript/packs/application.js
+++ 08-rails/tunr/app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 
+import 'bootstrap/dist/js/bootstrap';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
