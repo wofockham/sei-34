8269a4e79e20b41ab8cdf74a5769193228b0a76d
JT <certainthings@gmail.com>
2019-11-11 13:01:39 +1100

    tdd part ii

--- 09-tdd/bank/bank.rb
+++ 09-tdd/bank/bank.rb
@@ -9,4 +9,16 @@ class Bank
   def create_account(name, deposit)
     @accounts[name] = deposit
   end
+
+  def deposit(name, amount)
+    @accounts[name] += amount
+  end
+
+  def withdraw(name, amount)
+    @accounts[name] -= amount unless amount > @accounts[name]
+  end
+
+  def balance(name)
+    @accounts[name]
+  end
 end
