7d6430f8790707b5813e3a71fe5a764ae1866ce5
JT <certainthings@gmail.com>
2019-11-14 15:21:07 +1100

    elm intro

---
+++ 12-react/elm-intro/src/01-buttons.elm
@@ -0,0 +1,32 @@
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+main = Html.beginnerProgram { model = model, view = view, update = update }
+
+-- model
+type alias Model = Int
+model : Model
+model = 0
+
+-- update
+type Msg = Increment | Decrement | Reset
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+    Decrement ->
+      model - 1
+    Reset ->
+      0
+
+-- view
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "reset" ]
+    ]
---
+++ 12-react/elm-intro/src/02-text-input.elm
@@ -0,0 +1,28 @@
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+main = Html.beginnerProgram { model = model, view = view, update = update }
+
+-- model
+type alias Model = { content : String }
+
+model : Model
+model = { content = "" }
+
+-- update
+type Msg = Change String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+-- view
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "Text to reverse", onInput Change ] []
+    , div [] [ text (String.reverse model.content) ]
+    ]
