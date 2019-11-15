cf01e25611f687644c96112170b946dd8e00c2cd
JT <certainthings@gmail.com>
2019-11-15 12:56:59 +1100

    <HelloReact>

---
+++ 12-react/hello-react/src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'; // destructuring
+
+class HelloWorld extends Component {
+  // Every React class component must have method called render()
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default HelloWorld;
