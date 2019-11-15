aea9abf3d194d9fe04d3bb690432de304be958cb
JT <certainthings@gmail.com>
2019-11-15 12:57:48 +1100

    fibonnaci recursive

--- 13-comp-sci/recursion/fibonacci.rb
+++ 13-comp-sci/recursion/fibonacci.rb
@@ -4,13 +4,18 @@ def fibonacci_iterative(n)
   while n > 2
     # parellel assignment
     a, b = b, a + b # atomic
-    n -= 1 # move towards the base case
+    n -= 1 # mutation: move towards the base case
   end
 
   b
 end
 
-def fibonacci_recursive
+def fibonacci_recursive(n)
+  if n == 1 || n == 2
+    1
+  else
+    fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+  end
 end
 
 require 'pry'
