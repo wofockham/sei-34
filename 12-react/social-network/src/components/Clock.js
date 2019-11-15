3d251fa7d9032c5684411ac63d38843493b8707c
JT <certainthings@gmail.com>
2019-11-15 15:16:57 +1100

    react clock with setInterval example

--- 12-react/social-network/src/components/Clock.js
+++ 12-react/social-network/src/components/Clock.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+// TODO:
+// Create in another a <Time /> component that shows how many seconds the user loaded the page.
+// You loaded this page 19 seconds ago.
+
 class Clock extends Component {
   constructor() {
     super();
     this.state = { time: (new Date()).toString() };
+
+    setInterval(() => {
+      this.setState({time: (new Date()).toString() });
+    }, 1000);
   }
 
   render() {
