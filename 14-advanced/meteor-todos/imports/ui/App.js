f0f38648ca435c7a0e408865f5001be3a92e4815
JT <certainthings@gmail.com>
2019-12-03 12:35:57 +1100

    meteor todos completed

--- 14-advanced/meteor-todos/imports/ui/App.js
+++ 14-advanced/meteor-todos/imports/ui/App.js
@@ -7,6 +7,19 @@ import { Tasks } from '../api/tasks.js'; // !!
 import Task from './Task.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
@@ -21,7 +34,11 @@ class App extends Component {
   }
 
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.completed);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -30,7 +47,16 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)} />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input
@@ -52,6 +78,7 @@ class App extends Component {
 // Magical Higher Order Function
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch()
+    tasks: Tasks.find({}, {sort: { createdAt: -1} }).fetch(),
+    incompleteCount: Tasks.find({ completed: { $ne: true } }).count()
   }
 })(App);
--- 14-advanced/meteor-todos/imports/ui/Task.js
+++ 14-advanced/meteor-todos/imports/ui/Task.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 export default class Task extends Component {
+  toggleCompleted() {
+    Tasks.update(this.props.task._id, {
+      $set: { completed: !this.props.task.completed }
+    })
+  }
+
+  deleteTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    const taskClassName = this.props.task.completed ? 'checked' : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={ taskClassName }>
+        <button className="delete" onClick={this.deleteTask.bind(this)}>
+          &times;
+        </button>
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.completed}
+          onClick={this.toggleCompleted.bind(this)} />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
