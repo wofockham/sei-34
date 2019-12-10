5cae2fa6d465534d98881e6fd3541016b8609a9f
JT <certainthings@gmail.com>
2019-12-11 10:44:13 +1100

    don martin sound effect scraper

---
+++ 14-advanced/don_martin/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs'); // Node standard library.
+const axios = require('axios');
+const $ = require('cheerio');
+
+const sourceURL = 'https://www.madcoversite.com/dmd-alphabetical.html';
+
+axios.get( sourceURL ).then((response) => {
+  const html = response.data;
+  const $rows = $('table table tr:nth-child(n+2)', html);
+
+  const entries = [];
+
+  $rows.each(function () {
+    const $cells = $('td', this); // An array of 4 <td> cells
+    const entry = {
+      sound: $cells.eq(0).text(),
+      description: $cells.eq(1).text(),
+      source: $cells.eq(2).text(),
+      title: $cells.eq(3).text()
+    };
+
+    entries.push(entry);
+  });
+
+  // Pretty printing with 4 spaces of indentation
+  fs.writeFile('./martin.json', JSON.stringify( entries, null, 4 ), () => {
+    console.log(`martin.json saved: (${ entries.length } entries)`);
+  });
+});
